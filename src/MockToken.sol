3a34957311e279ffa3ff1622eaabbe8903d999c4
dcntrl <dcntrlx@gmail.com>
2025-11-23 16:40:48 +0300

    Fixes in MockToken

--- src/MockToken.sol
+++ src/MockToken.sol
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.30
+pragma solidity ^0.8.30;
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol"
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MockToken is ERC20 {  // Creating a mock toke for vault testing
+contract MockToken is
+    ERC20 // Creating a mock toke for vault testing
+{
     constructor() ERC20("MockToken", "MTK") {
-        _mint(msg.sender, 1000000 * 10**decimals());
+        _mint(msg.sender, 1000000 * 10 ** decimals());
     }
 }
