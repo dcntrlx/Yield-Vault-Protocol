a878a4667669523c2ef3dc7212b3392f97ce18b8
dcntrl <dcntrlx@gmail.com>
2025-11-25 12:33:47 +0300

    Implemented full realization of EIP4626 Vault interface

---
+++ docs/buffer.md
@@ -0,0 +1,11 @@
+
+Make full instance of ERC20 working using OZ
+
+Postponable
+difference between constant and immutable
+ERC777
+ERC1820
+ERC-7674
+Panic
+SafeCast
+continue Math
--- src/Vault.sol
+++ src/Vault.sol
@@ -6,8 +6,11 @@ import {IERC4626} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
+
 contract Vault is ERC20, IERC4626 {
     using SafeERC20 for IERC20;
+    using Math for uint256;
 
     IERC20 private immutable _asset; // Definition of the asset token
 
@@ -15,11 +18,96 @@ contract Vault is ERC20, IERC4626 {
         _asset = assetToken_;
     }
 
-    function asset() external view override returns (address) {
-        return address(_asset);
+    function asset() external view override returns (address assetTokenAddress) {
+        assetTokenAddress = address(_asset);
     }
 
-    function totalAssets() external view override returns (uint256) {
-        return _asset.balanceOf(address(this));
+    function totalAssets() external view returns (uint256 totalManagedAssets) {
+        totalManagedAssets = _asset.balanceOf(address(this)); // May be not correct if tokens during strategy are sent to different place
+    }
+
+    function convertToShares(uint256 assets) public view override returns (uint256 shares) {
+        shares = _convertToShares(assets, Math.Rounding.Floor);
+    }
+
+    function convertToAssets(uint256 shares) public view override returns (uint256 assets) {
+        assets = _convertToAssets(shares, Math.Rounding.Floor);
+    }
+
+    // Deposit block
+    function maxDeposit(address receiver) public view override returns (uint256 maxAssets) {
+        maxAssets = 2 ** 256 - 1;
+    }
+
+    function previewDeposit(uint256 assets) external view returns (uint256 shares) {
+        shares = _convertToShares(assets, Math.Rounding.Floor);
+    }
+
+    function deposit(uint256 assets, address receiver) external returns (uint256 shares) {
+        require(assets <= maxDeposit(receiver));
+        shares = _convertToShares(assets, Math.Rounding.Floor);
+        require(shares > 0, "Your assets is not enough to mint a single share"); // User protection. Not required by EIP4626
+        _asset.safeTransferFrom(msg.sender, address(this), assets);
+        _mint(receiver, shares);
+        emit Deposit(msg.sender, receiver, assets, shares);
+    }
+
+    // Mint block
+    function maxMint(address receiver) public view returns (uint256 maxShares) {
+        maxShares = 2 * 256 - totalSupply();
+    }
+
+    function previewMint(uint256 shares) external view returns (uint256 assets) {
+        assets = _convertToAssets(shares, Math.Rounding.Ceil);
+    }
+
+    function mint(uint256 shares, address receiver) external returns (uint256 assets) {
+        require(shares <= maxMint(receiver), "Shares to be minted will exceed the limit");
+        assets = _convertToAssets(shares, Math.Rounding.Ceil);
+        _asset.saveTransferFrom(msg.sender, address(this), assets);
+        _mint(receiver, shares);
+        emit Deposit(msg.sender, receiver, assets, shares);
+    }
+
+    // Withdraw block
+    function maxWithdraw(address owner) external view returns (uint256 maxAssets) {
+        maxAssets = _convertToAssets(balanceOf(owner), Math.Rounding.Floor);
+    }
+
+    function previewWithdraw(uint256 assets) external view returns (uint256 shares) {
+        shares = _convertToShares(assets, Math.Rounding.Ceil);
+    }
+
+    function withdraw(uint256 assets, address receiver, address owner) external returns (uint256 shares) {
+        shares = _convertToShares(assets, Math.Rounding.Ceil);
+        require(balanceOf(owner) >= shares, "You dont have enough shares to withdraw this amount of assets");
+        _burn(owner, shares);
+        saveTransfer(receiver, assets);
+        emit Withdraw(msg.sender, receiver, owner, assets, shares);
+    }
+
+    // Redeem block
+    function maxRedeem(address owner) external view returns (uint256 maxShares) {
+        maxShares = balanceOf(owner);
+    }
+
+    function previewRedeem(uint256 shares) external view returns (uint256 assets) {
+        assets = _convertToAssets(shares, Math.Rounding.Floor);
+    }
+
+    function redeem(uint256 shares, address receiver, address owner) external returns (uint256 assets) {
+        assets = _convertToAssets(shares, Math.Rounding.Floor);
+        require(assets >= 0, "You will not get any single share for this amount of assets");
+        _burn(owner, shares);
+        safeTransfer(receiver, assets);
+        emit Withdraw(msg.sender, receiver, owner, assets, shares);
+    }
+
+    function _convertToShares(uint256 assets) internal view returns (uint256 shares, Math.Rounding rounding) {
+        shares = assets.mulDiv(totalSupply() + 1, totalAssets() + 1, rounding);
+    }
+
+    function _convertToAssets(uint256 shares, Math.Rounding rounding) internal view returns (uint256 assets) {
+        assets = shares.mulDiv(totalAssets() + 1, totalSupply() + 1, rounding);
     }
 }
